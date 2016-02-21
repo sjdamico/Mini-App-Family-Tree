17160ee9db3ac4bc1283ed24f567ec43ee36cfe7
StevesRepository <sjdamico203@sbcglobal.net>
2016-02-20 23:09:40 -0600

    Initial Commit

---
+++ Mini App Family Tree/RootViewController.swift
@@ -0,0 +1,27 @@
+//
+//  RootViewController.swift
+//  Mini App Family Tree
+//
+//  Created by Steve D'Amico on 2/18/16.
+//  Copyright © 2016 Steve D'Amico. All rights reserved.
+//
+
+import UIKit
+
+class RootViewController: UIViewController {
+    // View Controller properties
+    private var rootViewController: RootViewController!
+    private var stevesParentViewController: StevesParentViewController!
+    private var jansParentViewController: JansParentViewController!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+}
---
+++ Mini App Family TreeTests/Mini_App_Family_TreeTests.swift
@@ -0,0 +1,36 @@
+//
+//  Mini_App_Family_TreeTests.swift
+//  Mini App Family TreeTests
+//
+//  Created by Steve D'Amico on 2/18/16.
+//  Copyright © 2016 Steve D'Amico. All rights reserved.
+//
+
+import XCTest
+@testable import Mini_App_Family_Tree
+
+class Mini_App_Family_TreeTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
